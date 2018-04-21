e252aec03835bf890a2f50dc888641e299b18276
unknown <ahmedtazi@hotmail.com>
2018-04-21 16:32:28 +0200

    Mandaroy Task Visualization Module

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# module7_visualization
+## VISUALIZATION EXAMPLE - JAVASCRIPT
+
+In this example, we will use JavaScript Language to create a BarChart with its corresponding legend.
+
+This example is inspired from the content provided by Braulio Diez @**Lemoncode**, you can check it out and also others projects in the following link: 
+
+Git: [Lemoncode](https://github.com/Lemoncode)
+
+WebPage: [LemoncodeWebPage](http://lemoncode.net/)
+
+### Description:
+The code provided covers the following features:
+
+- Adding space between columns,
+- Adding colors to each bar,
+- Adding a legend to our chart,
+- Show the BarChart vertically.
+
+_______________________
+
+### Contact:
+**Ahmed TAZI MEZALEK**
+
+    Graduate in MARKETING STRATEGIES & MARKET RESEARCH
+    Master in ADVANCED ANALYTICS ON BIG DATA
+
+
+For further information, please send and email to:
+    **ahmedtazi@hotmail.com**
---
+++ main.js
@@ -0,0 +1,129 @@
+
+//*Define the color of each bar/product
+var totalSales = [
+    { product: 'Hoodie', sales: 12 , "color" : "MediumPurple"},
+    { product: 'Jacket', sales: 4 ,"color" : "yellow"},
+    { product: 'Snuggie', sales: 7 ,"color" : "MediumTurquoise"},
+    ];
+
+var svg = d3.select('svg');
+
+var rects = svg.selectAll('rect')
+  .data(totalSales);
+
+var maxSales = d3.max(totalSales, function(d, i) {
+  return d.sales;
+});
+
+
+main();
+
+function main() { 
+  setupCanvasSize();
+  appendSvg("body");
+// Add the scale for products and sales
+  setupXScale();
+  setupYScale();
+// Add the axis X and Y
+  appendXAxis();
+  appendYAxis();
+  appendChartBars();
+  addLegend();
+};
+
+
+function setupCanvasSize() {
+    margin = {top: 40, left: 50, bottom: 80 , right: 40};
+    width = 450 - margin.left - margin.right;
+    height = 600 - margin.top - margin.bottom;
+  }
+  
+function appendSvg(domElement) {
+    svg = d3.select(domElement).append("svg")
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
+                .append("g")
+                .attr("transform",
+                "translate(" + margin.left + "," + margin.top + ")");
+                ;
+  }
+    
+function setupXScale()
+  {
+    x = d3.scaleBand()
+      .rangeRound([0, width])
+      .domain(totalSales.map(function(d, i) {
+        return d.product;
+      }))
+  //* Add space between bars
+      .paddingInner(0.10)
+  }
+  
+  
+function setupYScale()
+  {
+    var maxSales = d3.max(totalSales, function(d, i) {
+      return d.sales;
+    });
+  
+    y = d3.scaleLinear()
+      .range([height,0])
+      .domain([0, maxSales]);    
+  }
+
+function appendXAxis() 
+  {
+    svg.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x));
+  }
+
+function appendYAxis() 
+{
+    svg.append("g")   
+    .call(d3.axisLeft(y));
+}
+
+function appendChartBars()
+{
+  var rects = svg.selectAll('rect')
+    .data(totalSales);
+
+  var newRects = rects.enter();
+  newRects.append('rect')
+      .attr('x', function(d, i) {
+        return x(d.product);
+      })
+      .attr('y', function(d) {
+        return y(d.sales);
+      })     
+      .attr('height', function(d, i) {
+        return height - y(d.sales);
+      })
+      .attr('width', x.bandwidth) 
+      //* Change bar colors 
+      .style("fill", function(d) {return d.color});     
+      ;
+}
+
+function addLegend()  {
+  //*Add a legend to the barchar
+    var legend = svg.selectAll(".legend")
+      .data(totalSales.slice())
+      .enter().append("g")
+      .attr("class", "legend")
+      .attr("transform", function (d, i) { return "translate(0," + i * 15 + ")"; });
+  
+    legend.append("rect")
+      .attr("x", 300)
+      .attr("width", 10)
+      .attr("height", 15)
+      .style("fill", function (d) {return d.color});
+    
+    legend.append("text")
+      .attr("x", 270)
+      .attr("y", 7)
+      .attr("dy", ".35em")
+      .style("text-anchor", "middle")
+      .text(function (d) { return d.product; });
+    } 
